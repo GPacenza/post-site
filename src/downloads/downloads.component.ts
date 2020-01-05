676c03026d3704d8b9cec8cb3cd913d88cfd2372
Giancarlo Pacenza <giancarlopacenza@Giancarlos-MacBook-Pro.local>
2020-01-05 11:01:18 -0500

    fixed downloads component

--- src/downloads/downloads.component.ts
+++ src/downloads/downloads.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import * as request from 'request-promise-native';
 
+import { environment } from './../environments/environment';
+const firebase = require("firebase");
+require("firebase/firestore");
+
 @Component({
   selector: 'my-downloads',
   templateUrl: './downloads.component.html',
@@ -13,55 +17,39 @@ export class DownloadsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    //this.getData();
   }
 
   async getData(){
-    var url = "https://www.googleapis.com/fusiontables/v2/query?sql=SELECT%20*%20FROM%201lgxjPxZ8_V6OyA0Wa19iGpuZKKn1mBf1chJOgkqU%20WHERE%20plant%3D%27" + localStorage.getItem("plantName") + "%27%20AND%20rawWaterTurbidity%20>%200%20ORDER%20BY%20timeFinished%20DESC&key=AIzaSyB4fY4TPsWMhqifu68GFq1aWREjiiAYZmo";
-    // var url2 = "https://www.googleapis.com/fusiontables/v2/query?sql=SELECT%20*%20FROM%201lgxjPxZ8_V6OyA0Wa19iGpuZKKn1mBf1chJOgkqU%20WHERE%20plant%3D%27" + localStorage.getItem("plantName") + "%27%20AND%20rawWaterTurbidity%20>%200%20ORDER%20BY%20timeFinished%20DESC&key=AIzaSyB4fY4TPsWMhqifu68GFq1aWREjiiAYZmo";
-    const result = await request.get(url);
-    var data = JSON.parse(result);
-    var columns = data.columns;
-    var data_rows = data.rows;
-    var timeIndex= columns.indexOf("timeFinished");
-    var rwtIndex = columns.indexOf("rawWaterTurbidity");
-    var swtIndex = columns.indexOf("settledWaterTurbidity");
-    var fwtIndex = columns.indexOf("filteredWaterTurbidity1");
-    var cdIndex = columns.indexOf("coagulantDose");
-    for (var i=0; i<data_rows.length; i++){
-      var row = data_rows[i];
-      var csv_row = new Array([row[timeIndex], row[rwtIndex], row[swtIndex],row[fwtIndex], row[cdIndex]])
-      this.content.push(csv_row)
-    }
-    let csvContent = "data:text/csv;charset=utf-8";
-    csvContent += "filler, Time Finished,Raw Water Turbidity (NTU),Settled Water Turbidity (NTU), Filtered Water Turbidity (NTU), Coagulant Dose (mg/L)\n";
-    for (var j=0; j<this.content.length; j++){
-      var entry = this.content[j].join(",");
-      csvContent += entry + "\n"
-    }
+    var db = firebase.database();
+    var plant = localStorage.getItem("plantName").replace(/ /g, "");
+    const ref = db.ref(plant);
+    var self = this;
 
-    var encoded = encodeURI(csvContent);
-    var link = document.createElement("a");
-    link.setAttribute("href", encoded);
-    link.setAttribute("download", localStorage.getItem("plantName") + "_Data.csv");
-    document.body.appendChild(link);
-    link.click();
+    ref.on('value', function(snapshot) {
+      var nodeData;
+      var entries = []
+
+      snapshot.forEach(function(childSnapshot) {
+        nodeData = childSnapshot.val();
+        var csv_row = new Array([nodeData["timeFinished"], nodeData['rawWaterTurbidity'], nodeData['settledWaterTurbidity'], nodeData['filteredWaterTurbidity1'], nodeData['coagulantDose']])
+        self.content.push(csv_row);
+      });
+
+      var csvContent = "data:text/csv;charset=utf-8";
+      csvContent += "filler, Time Finished,Raw Water Turbidity (NTU),Settled Water Turbidity (NTU), Filtered Water Turbidity (NTU), Coagulant Dose (mg/L)\n";
+      for (var j=0; j<self.content.length; j++){
+        var entry = self.content[j].join(",");
+        csvContent += entry + "\n"
+      }
+
+      var encoded = encodeURI(csvContent);
+      var link = document.createElement("a");
+      link.setAttribute("href", encoded);
+      link.setAttribute("download", localStorage.getItem("plantName") + "_Data.csv");
+      document.body.appendChild(link);
+      link.click();
+
+    }
   }
-
-  // downloadData(){
-  //   let csvContent = "data:text/csv;charset=utf-8";
-  //   csvContent += "filler, Time Finished,Raw Water Turbidity (NTU),Settled Water Turbidity (NTU), Filtered Water Turbidity (NTU), Coagulant Dose (mg/L)\n";
-  //   for (var j=0; j<this.content.length; j++){
-  //     var entry = this.content[j].join(",");
-  //     csvContent += entry + "\n"
-  //   }
-  //
-  //   var encoded = encodeURI(csvContent);
-  //   var link = document.createElement("a");
-  //   link.setAttribute("href", encoded);
-  //   link.setAttribute("download", localStorage.getItem("plantName") + "_Data.csv");
-  //   document.body.appendChild(link);
-  //   link.click();
-  // }
 
 }
--- src/table/table.component.ts
+++ src/table/table.component.ts
@@ -28,7 +28,6 @@ export class TableComponent implements OnInit {
 
     var db = firebase.database();
     var plant = localStorage.getItem("plantName").replace(/ /g, "");
-    console.log("plant => ", plant);
     const ref = db.ref(plant);
     var self = this;
 
@@ -42,31 +41,5 @@ export class TableComponent implements OnInit {
         self.rows.push(<JSON>obj);
       });
     }
-
-
-
-
-
-
-
-
-    // var url = "https://www.googleapis.com/fusiontables/v2/query?sql=SELECT%20*%20FROM%201lgxjPxZ8_V6OyA0Wa19iGpuZKKn1mBf1chJOgkqU%20WHERE%20plant%3D%27" + localStorage.getItem("plantName") + "%27%20AND%20rawWaterTurbidity%20>%200%20ORDER%20BY%20timeFinished%20DESC%20LIMIT%20100&key=AIzaSyB4fY4TPsWMhqifu68GFq1aWREjiiAYZmo";
-    // const result = await request.get(url);
-    // var data = JSON.parse(result);
-    // var columns = data.columns;
-    // var data_rows = data.rows;
-    // console.log(data.columns);
-    // console.log(columns.indexOf("timeFinished"));
-    // var timeIndex= columns.indexOf("timeFinished");
-    // var rwtIndex = columns.indexOf("rawWaterTurbidity");
-    // var swtIndex = columns.indexOf("settledWaterTurbidity");
-    // var fwtIndex = columns.indexOf("filteredWaterTurbidity1");
-    // var cdIndex = columns.indexOf("coagulantDose");
-    // for (var i=0; i<data_rows.length; i++){
-    //   var row = data_rows[i];
-    //   var obj:any = {"timeStamp": row[timeIndex], "rawWaterTurbidity": row[rwtIndex], "settledWaterTurbidity": row[swtIndex], "filteredWaterTurbidity": row[fwtIndex], "coagulantDose": row[cdIndex]};
-    //   this.rows.push(<JSON>obj);
-    // }
-
-}
+  }
 }
